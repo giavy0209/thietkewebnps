4a252cdebbaef10601dbab29ca2fa2c23d89e21f
giavy0209 <49144082+giavy0209@users.noreply.github.com>
2019-06-06 13:36:10 +0700

    a

--- js/main.js
+++ js/main.js
@@ -11,42 +11,62 @@ body.addEventListener('wheel',function(event){
     var y = event.deltaY;
 
     
-    console.log(indexOfActive, section.length)
-    if(y > 0){
-        var indexOfActive = section.indexOf(sectionActive) + 1;
-        var index = 0;
-        if(sectionActive.nextElementSibling != null){
-            sectionActive.nextElementSibling.classList.add('active');
-            sectionActive.classList.remove('active')
-            // sectionActive.style.left = "-101%"
-            for(indexOfActive; indexOfActive < section.length; indexOfActive++){
-                section[indexOfActive].style.left = 101 * index + "%";
-                index++;
-            }
-            var indexOfActive = section.indexOf(sectionActive);
-            var index = 1;
-            for(indexOfActive; indexOfActive >= 0; indexOfActive--){
-                section[indexOfActive].style.left = "-" + (101 * index) + "%";
-                index++;
-            }
-        }
-    }
-    if(y < 0){
-        var indexOfActive = section.indexOf(sectionActive) - 1;
-        var index = 0;
-        if(sectionActive.previousElementSibling != null){
-            sectionActive.previousElementSibling.classList.add('active');
-            sectionActive.classList.remove('active');
-            for(indexOfActive; indexOfActive < section.length; indexOfActive++){
-                section[indexOfActive].style.left = 101 * index + "%";
-                index++;
-            }
-            var indexOfActive = section.indexOf(sectionActive) -2;
-            var index = 1;
-            for(indexOfActive; indexOfActive >= 0; indexOfActive--){
-                section[indexOfActive].style.left = "-" + (101 * index) + "%";
-                index++;
+    setTimeout(function(){
+        if(y > 0){
+            var indexOfActive = section.indexOf(sectionActive) + 1;
+            var index = 0;
+            if(sectionActive.nextElementSibling != null){
+                sectionActive.nextElementSibling.classList.add('active');
+                sectionActive.classList.remove('active')
+                for(indexOfActive; indexOfActive < section.length; indexOfActive++){
+                    section[indexOfActive].style.left = 101 * index + "%";
+                    index++;
+                }
+                var indexOfActive = section.indexOf(sectionActive);
+                var index = 1;
+                for(indexOfActive; indexOfActive >= 0; indexOfActive--){
+                    section[indexOfActive].style.left = "-" + (101 * index) + "%";
+                    index++;
+                }
+            }else{
+                // var indexOfActive = section.indexOf(sectionActive) + 1;
+                // var index = 0;
+                section[0].classList.add('active')
+                section[section.length - 1].classList.remove('active');
+                section.forEach(function(el,index){
+                    el.style.left = 101 * index + "%";
+                })
             }
         }
-    }
+    },300)
+
+    setTimeout(function(){
+        if(y < 0){
+            var indexOfActive = section.indexOf(sectionActive) - 1;
+            var index = 0;
+            if(sectionActive.previousElementSibling != null){
+                sectionActive.previousElementSibling.classList.add('active');
+                sectionActive.classList.remove('active');
+                for(indexOfActive; indexOfActive < section.length; indexOfActive++){
+                    section[indexOfActive].style.left = 101 * index + "%";
+                    index++;
+                }
+                var indexOfActive = section.indexOf(sectionActive) -2;
+                var index = 1;
+                for(indexOfActive; indexOfActive >= 0; indexOfActive--){
+                    section[indexOfActive].style.left = "-" + (101 * index) + "%";
+                    index++;
+                }
+            }else{
+                // debugger
+                section[section.length - 1].classList.add('active')
+                section[0].classList.remove('active');
+                var i = 0;
+                for (let index = section.length - 1; index >= 0; index--) {
+                    section[index].style.left = "-" + (101 * i) + "%";
+                    i++;
+                }
+            }
+        }
+    },300)
 }) 
